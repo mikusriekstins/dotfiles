825cf5c7d1e65b778284ece3b187846b81562bbe
Mikus Riekstins <mikus.riekstins@gmail.com>
2026-07-17 19:31:22 +0300

    Add emacs config

---
+++ emacs/init.el
@@ -0,0 +1,20 @@
+(org-babel-load-file
+  (expand-file-name
+  "config.org"
+  user-emacs-directory))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-enabled-themes '(everblush))
+ '(custom-safe-themes
+   '("9584533e7ca091a59f88e7f2acc2f8ce9124753d7b82aad6d4526ccf77787975"
+     default))
+ '(package-selected-packages nil))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
