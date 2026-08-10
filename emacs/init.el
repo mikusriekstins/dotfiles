0caa17f0f99d5f75240cc19e9d3348104ee9462e
Mikus Riekstins <mikus.riekstins@gmail.com>
2026-08-10 08:56:51 +0300

    Evil for emacs

--- emacs/init.el
+++ emacs/init.el
@@ -9,13 +9,15 @@
  ;; If there is more than one, they won't work right.
  '(custom-enabled-themes '(solarized-dark-high-contrast))
  '(custom-safe-themes
-   '("9c6aa7eb1bde73ba1142041e628827492bd05678df4d9097cda21b1ebcb8f8b9"
+   '("615d8f3208b19161ada77e155beaca0678a35abb182ceae87e26660ecbca04d5"
+     "46433a4be67ff0f415175d0876ef9435d190f3aa57bb00670febd70ba0ba52e1"
+     "9c6aa7eb1bde73ba1142041e628827492bd05678df4d9097cda21b1ebcb8f8b9"
      "9584533e7ca091a59f88e7f2acc2f8ce9124753d7b82aad6d4526ccf77787975"
      default))
  '(package-selected-packages
-   '(dracula-theme ef-themes ef-themesg exec-path-from-shell lsp-ui magit
-		   marginalia orderless org-journal rustic solarized-theme
-		   typescript-mode vertico vterm)))
+   '(consult corfu dracula-theme ef-themes evil exec-path-from-shell
+	     ghostel lsp-ui magit marginalia orderless org-journal
+	     rustic solarized-theme typescript-mode vertico vterm)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
